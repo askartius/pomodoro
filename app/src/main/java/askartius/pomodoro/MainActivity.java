2a3dcae193de7409043206df5bd3f36f81d0d7cf
Askartius <151680098+askartius@users.noreply.github.com>
2024-06-11 22:17:09 +0300

    Display time in the center of the circle

--- app/src/main/java/askartius/pomodoro/MainActivity.java
+++ app/src/main/java/askartius/pomodoro/MainActivity.java
@@ -6,6 +6,8 @@ import android.media.RingtoneManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.DragEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +17,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
-    private final int focusTime = 10; // Focus time in seconds
+    private int time = 5 * 60; // Focus time in seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +34,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
         CircularProgressIndicator timeCircle = findViewById(R.id.time_circle);
-        timeCircle.setMax(focusTime);
+        timeCircle.setMax(time * 60);
 
-        SoundPool soundPool = new SoundPool.Builder().build();
-        int dingId = soundPool.load(this, R.raw.ding, 1);
+        MaterialTextView timeDisplay = findViewById(R.id.time_display);
+        timeDisplay.setText(String.valueOf(time / 60));
+        timeDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (time == 5) {
+                    time = 25;
+                } else {
+                    time = 5;
+                }
 
-        new CountDownTimer(focusTime * 1000, 1000) {
+                timeDisplay.setText(String.valueOf(time));
+            }
+        });
+        
+
+        SoundPool soundPool = new SoundPool.Builder()
+                .setAudioAttributes(
+                        new AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                                .setUsage(AudioAttributes.USAGE_ALARM)
+                                .build()
+                )
+                .build();
+        int ding = soundPool.load(this, R.raw.ding, 1);
+
+        new CountDownTimer(time * 60000L, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timeCircle.setProgress((int) (focusTime - millisUntilFinished / 1000), true);
+                timeCircle.setProgress((int) (time * 60 - millisUntilFinished / 1000));
             }
 
             @Override
             public void onFinish() {
-                Toast.makeText(MainActivity.this, "Focus finished, time to relax!", Toast.LENGTH_SHORT).show();
-
-
-                //soundPool.play(dingId, 1, 1, 1, 3, 1);
-                MediaPlayer player = MediaPlayer.create(getApplicationContext(), R.raw.ding);
-                for (int i = 0; i < 3; i++) {
-                    player.start();
-                }
+                Toast.makeText(MainActivity.this, "Focus finished, time to relax!", Toast.LENGTH_LONG).show();
+                soundPool.play(ding, 1, 1, 1, 3, 1);
             }
         }.start();
     }

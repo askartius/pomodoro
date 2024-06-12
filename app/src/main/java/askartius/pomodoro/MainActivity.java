a2275d8c94e896ef607d5c576cda79a9d34cfd88
Askartius <151680098+askartius@users.noreply.github.com>
2024-06-13 00:02:11 +0300

    Cleanup code

--- app/src/main/java/askartius/pomodoro/MainActivity.java
+++ app/src/main/java/askartius/pomodoro/MainActivity.java
@@ -19,10 +19,14 @@ public class MainActivity extends AppCompatActivity {
     private final String PROGRESS = "progress";
     private final String TIMER_TIME = "timerTime";
     private final String TIMER_RUNNING = "timerRunning";
+    private final int FOCUS_TIME = 1500;
+    private final int RELAX_TIME = 300;
     private int timerTime = 0;
     private boolean timerRunning = false;
+    private int dingSound;
     private CountDownTimer timer;
-    private CircularProgressIndicator timeCircle;
+    private CircularProgressIndicator timerCircle;
+    private SoundPool soundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +41,8 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        timeCircle = findViewById(R.id.time_circle);
-
-        SoundPool soundPool = new SoundPool.Builder()
+        timerCircle = findViewById(R.id.timer_circle);
+        soundPool = new SoundPool.Builder()
                 .setAudioAttributes(
                         new AudioAttributes.Builder()
                                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
@@ -47,65 +50,22 @@ public class MainActivity extends AppCompatActivity {
                                 .build()
                 )
                 .build();
-        int ding = soundPool.load(this, R.raw.ding, 1);
+        dingSound = soundPool.load(this, R.raw.ding, 1);
 
-        if (savedInstanceState != null) {
-            timerTime = savedInstanceState.getInt(TIMER_TIME);
-            timerRunning = savedInstanceState.getBoolean(TIMER_RUNNING);
-            timeCircle.setProgress(savedInstanceState.getInt(PROGRESS));
-            timeCircle.setMax(timerTime);
-
-            if (timerTime == 1500) { // If timerTime == focus time
-                timeCircle.setIndicatorColor(getColor(R.color.red));
-                timeCircle.setTrackColor(getColor(R.color.light_red));
-            } else {
-                timeCircle.setIndicatorColor(getColor(R.color.green));
-                timeCircle.setTrackColor(getColor(R.color.light_green));
-            }
-
-            if (timerRunning) {
-                timer = new CountDownTimer((timerTime - savedInstanceState.getInt(PROGRESS)) * 1000L, 1000L) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        timeCircle.setProgress((int) (timerTime - millisUntilFinished / 1000), true);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        timerRunning = false;
-                        soundPool.play(ding, 1, 1, 1, 2, 1);
-                        switchMode();
-                    }
-                }.start();
-            }
-        } else {
-            switchMode();
-        }
+        switchMode();
 
         // Switch mode on click
-        timeCircle.setOnClickListener(v -> switchMode());
+        timerCircle.setOnClickListener(v -> switchMode());
 
         // Start timer on long click
-        timeCircle.setOnLongClickListener(v -> {
+        timerCircle.setOnLongClickListener(v -> {
             if (timerRunning) {
                 timerRunning = false;
                 timer.cancel();
                 switchMode();
             } else {
                 timerRunning = true;
-                timer = new CountDownTimer(timerTime * 1000L, 1000L) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        timeCircle.setProgress((int) (timerTime - millisUntilFinished / 1000), true);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        timerRunning = false;
-                        soundPool.play(ding, 1, 1, 1, 2, 1);
-                        switchMode();
-                    }
-                }.start();
+                startTimer(timerTime);
             }
 
             return true;
@@ -123,27 +83,65 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt(PROGRESS, timeCircle.getProgress());
+        outState.putInt(PROGRESS, timerCircle.getProgress());
         outState.putInt(TIMER_TIME, timerTime);
         outState.putBoolean(TIMER_RUNNING, timerRunning);
 
         super.onSaveInstanceState(outState);
     }
 
-    private void switchMode() {
-        if (!timerRunning) {
-            if (timerTime == 1500) { // If timerTime == focus time
-                timerTime = 300;
-                timeCircle.setIndicatorColor(getColor(R.color.green));
-                timeCircle.setTrackColor(getColor(R.color.light_green));
-            } else {
-                timerTime = 1500;
-                timeCircle.setIndicatorColor(getColor(R.color.red));
-                timeCircle.setTrackColor(getColor(R.color.light_red));
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        timerTime = savedInstanceState.getInt(TIMER_TIME);
+        timerRunning = savedInstanceState.getBoolean(TIMER_RUNNING);
+        timerCircle.setProgress(savedInstanceState.getInt(PROGRESS));
+        timerCircle.setMax(timerTime);
+        updateUI();
+
+        if (timerRunning) {
+            startTimer(timerTime - timerCircle.getProgress());
+        }
+    }
+
+    private void startTimer(int time) {
+        timer = new CountDownTimer(time * 1000L, 1000L) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerCircle.setProgress((int) (timerTime - millisUntilFinished / 1000), true);
             }
 
-            timeCircle.setProgress(0, true);
-            timeCircle.setMax(timerTime);
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                soundPool.play(dingSound, 1, 1, 1, 2, 1);
+                switchMode();
+            }
+        }.start();
+    }
+
+    private void updateUI() {
+        if (timerTime == FOCUS_TIME) {
+            timerCircle.setIndicatorColor(getColor(R.color.red));
+            timerCircle.setTrackColor(getColor(R.color.light_red));
+        } else {
+            timerCircle.setIndicatorColor(getColor(R.color.green));
+            timerCircle.setTrackColor(getColor(R.color.light_green));
+        }
+    }
+
+    private void switchMode() {
+        if (!timerRunning) {
+            if (timerTime == FOCUS_TIME) {
+                timerTime = RELAX_TIME;
+            } else {
+                timerTime = FOCUS_TIME;
+            }
+
+            timerCircle.setProgress(0, true);
+            timerCircle.setMax(timerTime);
+            updateUI();
         }
     }
 }

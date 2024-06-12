1791dfb57dd962ece76c7e8e75ae3faa59ebd4e9
Askartius <151680098+askartius@users.noreply.github.com>
2024-06-12 12:18:29 +0300

    Simplified design Different colors for focus and relax modes

--- app/src/main/java/askartius/pomodoro/MainActivity.java
+++ app/src/main/java/askartius/pomodoro/MainActivity.java
@@ -4,9 +4,8 @@ import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,19 +13,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.color.DynamicColors;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 public class MainActivity extends AppCompatActivity {
-    private final int focusTime = 25 * 60;
-    private final int relaxTime = 5 * 60;
-    private int time = relaxTime; // Focus time in seconds
+    private int timerTime = 0;
+    private int timePassed = 0;
     private boolean timerRunning = false;
     private CountDownTimer timer;
+
+    CircularProgressIndicator timeCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -34,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        CircularProgressIndicator timeCircle = findViewById(R.id.time_circle);
-        timeCircle.setMax(time); // Update time circle's maximum time
+        timeCircle = findViewById(R.id.time_circle);
 
         SoundPool soundPool = new SoundPool.Builder()
                 .setAudioAttributes(
@@ -47,45 +50,57 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         int ding = soundPool.load(this, R.raw.ding, 1);
 
-        // Update time on click
-        timeCircle.setOnClickListener(v -> {
-            if (!timerRunning) {
-                if (time == focusTime) {
-                    time = relaxTime;
-                    Toast.makeText(MainActivity.this, "Relax (5 min)", Toast.LENGTH_SHORT).show();
-                } else {
-                    time = focusTime;
-                    Toast.makeText(MainActivity.this, "Focus (25 min)", Toast.LENGTH_SHORT).show();
-                }
-                timeCircle.setMax(time); // Update time circle's maximum time
-                Log.d("TEST", String.valueOf(time));
-            }
-        });
+        Handler handler = new Handler(getMainLooper());
+
+        switchMode();
+
+        // Switch mode on click
+        timeCircle.setOnClickListener(v -> switchMode());
 
         // Start timer on long click
         timeCircle.setOnLongClickListener(v -> {
             if (timerRunning) {
+                timerRunning = false;
                 timer.cancel();
-                timeCircle.setProgress(0);
+                switchMode();
             } else {
-                timer = new CountDownTimer(time * 1000L, 1000) {
+                timerRunning = true;
+                timer = new CountDownTimer(timerTime * 1000L, 1000L) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        timeCircle.setProgress((int) (time - millisUntilFinished / 1000));
+                        timeCircle.setProgress(timePassed);
+                        timePassed++;
                     }
 
                     @Override
                     public void onFinish() {
                         timerRunning = false;
-                        Toast.makeText(MainActivity.this, "Focus finished, time to relax!", Toast.LENGTH_LONG).show();
-                        soundPool.play(ding, 1, 1, 1, 3, 1);
+                        soundPool.play(ding, 1, 1, 1, 2, 1);
+                        switchMode();
                     }
                 }.start();
             }
 
-            timerRunning = !timerRunning;
-
             return true;
         });
+    }
+
+    private void switchMode() {
+        if (!timerRunning) {
+            timePassed = 0;
+            timeCircle.setProgress(timePassed);
+
+            if (timerTime == 1500) { // If timerTime == focus time
+                timerTime = 300;
+                timeCircle.setIndicatorColor(getColor(R.color.green));
+                timeCircle.setTrackColor(getColor(R.color.light_green));
+            } else {
+                timerTime = 1500;
+                timeCircle.setIndicatorColor(getColor(R.color.red));
+                timeCircle.setTrackColor(getColor(R.color.light_red));
+            }
+
+            timeCircle.setMax(timerTime); // Update time circle's maximum time
+        }
     }
 }
